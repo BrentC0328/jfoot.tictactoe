19b0baf8bc0d0cf8fe4d2fbfe56ec4418b615f63
xleonhunter <xleonhunter@gmail.com>
2022-07-29 16:34:12 -0400

    added method stubs

--- src/main/java/com/github/curriculeon/jfoot/TicTacToeBoard.java
+++ src/main/java/com/github/curriculeon/jfoot/TicTacToeBoard.java
@@ -1,7 +1,9 @@
 package com.github.curriculeon.jfoot;
 
-import com.github.git_leon.jfoot.resources.JFootTextImage;
 import greenfoot.*;  // imports Actor, World, Greenfoot, GreenfootImage
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * A world where wombats live.
@@ -22,14 +24,74 @@ public class TicTacToeBoard extends World {
     }
 
     public void populate() {
-        for(int xPosition=0; xPosition < getWidth(); xPosition++) {
-            for(int yPosition=0; yPosition < getHeight(); yPosition++) {
+        for (int xPosition = 0; xPosition < getWidth(); xPosition++) {
+            for (int yPosition = 0; yPosition < getHeight(); yPosition++) {
                 addObject(new Placeholder(), xPosition, yPosition);
             }
         }
     }
 
+    public Boolean isWinningDiagonallyTopRight(PlayerPiece pieceToEvaluate) {
+        PlayerPiece piece0301 = getObjectsAt(3, 1, PlayerPiece.class).get(0);
+        PlayerPiece piece0202 = getObjectsAt(2, 2, PlayerPiece.class).get(0);
+        PlayerPiece piece0103 = getObjectsAt(1, 3, PlayerPiece.class).get(0);
+        List<PlayerPiece> pieces = Arrays.asList(piece0301, piece0202, piece0103);
+        for(PlayerPiece piece : pieces) {
+            Class<?> actualClass = piece.getClass();
+            Class<?> expectedClass = pieceToEvaluate.getClass();
+            if(!actualClass.equals(expectedClass)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public Boolean isWinningDiagonallyTopLeft(PlayerPiece piece) {
+        return null;
+    }
+
+    public Boolean isWinningTopRow(PlayerPiece piece) {
+        return null;
+    }
+
+    public Boolean isWinningMiddleRow(PlayerPiece piece) {
+        return null;
+    }
+
+    public Boolean isWinningBottomRow(PlayerPiece piece) {
+        return null;
+    }
+
+    public Boolean isWinningLeftColumn(PlayerPiece piece) {
+        return null;
+    }
+
+    public Boolean isWinningMiddleColumn(PlayerPiece playerPiece) {
+        return false;
+    }
+
+    public Boolean isWinningRightColumn(PlayerPiece piece) {
+        return null;
+    }
+
+    public Boolean isWinning(PlayerPiece playerPiece) {
+        return isWinningTopRow(playerPiece) ||
+                isWinningBottomRow(playerPiece) ||
+                isWinningMiddleRow(playerPiece) ||
+                isWinningDiagonallyTopLeft(playerPiece) ||
+                isWinningDiagonallyTopRight(playerPiece) ||
+                isWinningRightColumn(playerPiece) ||
+                isWinningMiddleColumn(playerPiece) ||
+                isWinningLeftColumn(playerPiece);
+    }
+
     @Override
     public void act() {
+        List<PlayerPiece> pieces = getObjects(PlayerPiece.class);
+        for(PlayerPiece piece : pieces) {
+            if(isWinning(piece)) {
+                // TODO display winner
+            }
+        }
     }
 }
